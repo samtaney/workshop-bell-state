da2f782769e29d18abad0aba7770d23d94410660
Ravi Samtaney <samtanr@kl-21111.kaust.edu.sa>
2021-04-05 15:14:56 +0300

    Extra credit - smaller no of shots

--- steps/extra.py
+++ steps/extra.py
@@ -18,7 +18,7 @@ def main():
     # End quantum circuit
 
     # Execute with qiskit
-    result = execute(circuit, Aer.get_backend("qasm_simulator"), shots=10000).result()
+    result = execute(circuit, Aer.get_backend("qasm_simulator"), shots=100).result()
     counts = result.get_counts(circuit)
 
     with open("results.json", "w") as outfile:
