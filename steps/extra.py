688526eded8ba77a8cfe1945de3b85c7ba81b116
Ravi Samtaney <samtanr@kl-21111.kaust.edu.sa>
2021-04-05 15:01:34 +0300

    Extra credit

---
+++ steps/extra.py
@@ -0,0 +1,25 @@
+from qiskit import execute, Aer, ClassicalRegister, QuantumCircuit, QuantumRegister
+import json
+
+
+def main():
+    # Define registers and circuit
+    q = QuantumRegister(3)
+    c = ClassicalRegister(3)
+    circuit = QuantumCircuit(q, c)
+
+    # Quantum circuit starts here
+    # MISSING
+    # MISSING
+    circuit.h(q[0])
+    circuit.cnot(q[0], q[1])
+    circuit.cnot(q[0], q[1], q[2])
+    circuit.measure(q, c)
+    # End quantum circuit
+
+    # Execute with qiskit
+    result = execute(circuit, Aer.get_backend("qasm_simulator"), shots=10000).result()
+    counts = result.get_counts(circuit)
+
+    with open("results.json", "w") as outfile:
+        json.dump(counts, outfile)
